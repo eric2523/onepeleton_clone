a3a367ec30adc27e9671bb8b78098eb3fec24eda
eric2523@gmail.com <eric2523@gmail.com>
2020-10-11 16:02:33 -0700

    Create login form Create handleSubmit and handleChange event handlers

--- frontend/components/login_form/login_form.jsx
+++ frontend/components/login_form/login_form.jsx
@@ -4,12 +4,39 @@ class LoginForm extends React.Component {
   constructor(props){
     super(props)
     this.state = { username: "", password: "" }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(type) {
+    return (e) => {
+      e.preventDefault();
+      this.setState({ [type]: e.target.value })
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.processForm(this.state)
   }
 
   render(){
-    return(
-      <div>We're in the LoginForm</div>
-    )
+    return (
+      <div>
+        <form onSubmit={ this.handleSubmit }>
+          <input
+            onChange={this.handleChange("username")}
+            type="text"
+            value={this.state.username}
+          />
+          <input
+            onChange={this.handleChange("password")}
+            type="password"
+            value={this.state.password}
+          />
+          <input type="submit" value="Log In"/>
+        </form>
+      </div>
+    );
   }
 }
 
