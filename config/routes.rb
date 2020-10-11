639d9b3f24cff48221ec55507c3f77f24efd55f6
eric2523@gmail.com <eric2523@gmail.com>
2020-10-10 17:53:49 -0700

    Create Users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:index, :show, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
