5e16c4457ef14a22ce30d32b81ebb83123d964ca
eric2523@gmail.com <eric2523@gmail.com>
2020-10-11 16:10:43 -0700

    Create Errors component

---
+++ frontend/components/errors/errors.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Errors extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>Erors</div>
+    )
+  }
+}
+
+export default Errors;
