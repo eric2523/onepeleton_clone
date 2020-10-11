964a674560b9c8a10a53466af5850ced4a6e3edf
eric2523@gmail.com <eric2523@gmail.com>
2020-10-11 14:33:07 -0700

    Create testing window functions Add skip authenticity token to applican_controller and requre jquery in applican.js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   helper_method :logged_in?, :current_user, :require_log_in
 
+  skip_before_action :verify_authenticity_token
+
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
--- frontend/uno_peleton.jsx
+++ frontend/uno_peleton.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import configureStore from './store/store'
+
+//testing thunk action creators
+import { signup, login, logout } from './actions/session_actions/session_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root")
+  const store = configureStore();
 
   ReactDOM.render(<div>We are in React</div>, root)
+
+  //testing store on window
+  window.getState = store.getState
+  window.dispatch = store.dispatch
+  window.signup = signup
+  window.login = login
+  window.logout = logout
 
 })
