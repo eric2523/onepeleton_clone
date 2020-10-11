f1fd288c1e3dce5bfabfcb7f91c1c4290661ff59
eric2523@gmail.com <eric2523@gmail.com>
2020-10-11 14:02:54 -0700

    Add default state to sessionErrorReducer

--- frontend/reducers/session_reducers/session_error_reducer.js
+++ frontend/reducers/session_reducers/session_error_reducer.js
@@ -1,6 +1,6 @@
 import {RECEIVE_ERRORS, CLEAR_ERRORS} from "../../actions/session_actions/session_actions";
 
-const sessionErrorReducer = (state, action) => {
+const sessionErrorReducer = (state = {}, action) => {
   Object.freeze(state)
   switch (action.type) {
     case RECEIVE_ERRORS:
