2fe8c961db7d1c9ec5c2ea3a7f941758e2db0706
eric2523@gmail.com <eric2523@gmail.com>
2020-10-11 13:51:17 -0700

    Create receiveErrors action creator and signup thunk action creator

--- frontend/actions/session_actions/session_actions.js
+++ frontend/actions/session_actions/session_actions.js
@@ -2,6 +2,7 @@ import * as APIUtil from '../../util/session_api_util/session_api_util'
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
 
 const receiveCurrentUser = (user) => {
   return({
@@ -16,10 +17,18 @@ const logoutCurrentUser = () => {
   })
 }
 
+const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_ERRORS,
+    errors
+  };
+}
+
 export const login = (user) => {
   return (dispatch) => {
     return APIUtil.login(user)
       .then((user) => dispatch(receiveCurrentUser(user)))
+      .fail((errors) => dispatch(receiveErrors(errors.responseJSON)))
   }
 }
 
@@ -27,5 +36,14 @@ export const logout = () => {
   return (dispatch) => {
     return APIUtil.logout()
       .then(() => dispatch(logoutCurrentUser()))
+      .fail((errors) => dispatch(receiveErrors(errors.responseJSON)));
+  }
+}
+
+export const signup = (user) => {
+  return (dispatch) => {
+    return APIUtil.signup(user)
+      .then((user) => dispatch(receiveCurrentUser(user)))
+      .fail((errors) => dispatch(receiveErrors(errors.responseJSON)));
   }
 }
