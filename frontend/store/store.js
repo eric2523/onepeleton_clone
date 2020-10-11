607108c58deaae1e4ee26d2b123e0e944e843cbb
eric2523@gmail.com <eric2523@gmail.com>
2020-10-11 14:13:31 -0700

    Create configureStore file to create redux store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
+import rootReducer from '../reducers/root_reducer'
 
-// const configureStore = createStore()
+const configureStore = (preloadedSate = {}) => {
+  return createStore(
+    rootReducer,
+    preloadedSate,
+    applyMiddleware(thunk, logger)
+  );
+}
+
+export default configureStore;
