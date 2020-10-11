d1519d4a06166499f80507f5df3471a65235611c
eric2523@gmail.com <eric2523@gmail.com>
2020-10-11 14:52:38 -0700

    Create user actions file create receiveAllUsers and fetchAllUsers action creator and thunk action creator

---
+++ frontend/actions/user_actions/user_actions.js
@@ -0,0 +1,17 @@
+export const RECEIVE_ALL_USERS = "RECEIVE_ALL_USERS";
+export const FETCH_USER = "FETCH_USER";
+import * as UserAPIUtil from '../../util/user_api_util/user_api_util'
+
+const receiveAllUsers = (users) => {
+  return {
+    type: RECEIVE_ALL_USERS,
+    users
+  }
+}
+
+export const fetchAllUsers = () => {
+  return (dispatch) => {
+    return UserAPIUtil.fetchAllUsers()
+      .then((users) => dispatch(receiveAllUsers(users)))
+  }
+}
