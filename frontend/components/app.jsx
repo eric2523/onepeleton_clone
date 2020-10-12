c701f6188c0a982a13ab67b413ebeee1b9f54a33
eric2523@gmail.com <eric2523@gmail.com>
2020-10-11 19:47:47 -0700

    Add AuthRoutes to login and signup containers

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import LoginFormContainer from './login_form/login_form_container'
 import SignUpFormContainer from './signup_form/signup_form_container'
+import {AuthRoute} from '../util/route_util'
 
 const App = () => {
   return(
@@ -8,7 +9,8 @@ const App = () => {
       <header> 
         <h1>One Peleton Header</h1>
       </header>
-      <SignUpFormContainer />
+      <AuthRoute path="/login" component={LoginFormContainer} />
+      <AuthRoute path="/signup" component={SignUpFormContainer} />
     </div>
   )
 }
