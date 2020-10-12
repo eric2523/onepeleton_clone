53545a9cc6f05c294bf133b08c4f060e6ccae566
eric2523@gmail.com <eric2523@gmail.com>
2020-10-12 15:39:20 -0700

    Create categories model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_223513) do
+ActiveRecord::Schema.define(version: 2020_10_12_223741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
 
   create_table "classes", force: :cascade do |t|
     t.string "name", null: false
