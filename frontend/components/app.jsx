254087627f560d6dfc47b3a78f7cec2354c75ca2
eric2523@gmail.com <eric2523@gmail.com>
2020-10-12 18:35:32 -0700

    Add switcth to auth/protected routes protect from users typing in random route. If route doesnt exist will redirect to /classes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import LoginFormContainer from './login_form/login_form_container'
 import SignUpFormContainer from './signup_form/signup_form_container'
 import {AuthRoute, ProtectedRoute} from '../util/route_util'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import ClassContainer from './classes/class_container'
 import LightIconIndex from './social_media_icons/light_icon_index'
 
 const App = () => {
-
-
   return (
     <div className="session-background">
       <header>
@@ -16,10 +14,12 @@ const App = () => {
       </header>
 
       <div className="component-body">
-        <AuthRoute path="/login" component={LoginFormContainer} />
-        <AuthRoute path="/signup" component={SignUpFormContainer} />
-        <ProtectedRoute exact path="/" component={ClassContainer} />
-        <ProtectedRoute path="/classes" component={ClassContainer} />
+        <Switch>
+          <AuthRoute path="/login" component={LoginFormContainer} />
+          <AuthRoute path="/signup" component={SignUpFormContainer} />
+          <ProtectedRoute path="/classes" component={ClassContainer} />
+          <ProtectedRoute path="/" component={ClassContainer} />
+        </Switch>
       </div>
 
       <footer className="media-icon-div">
