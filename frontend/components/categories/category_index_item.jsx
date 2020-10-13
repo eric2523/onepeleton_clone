efe8cb1e263bb0a1d49acc0503618eab1d97efd5
eric2523@gmail.com <eric2523@gmail.com>
2020-10-13 15:10:26 -0700

    Add handleClick function for CategoryIndexItem redirects to ClassesIndex

--- frontend/components/categories/category_index_item.jsx
+++ frontend/components/categories/category_index_item.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 class CategoryIndexItem extends React.Component {
   constructor(props){
     super(props)
+    this.handleClick = this.handleClick.bind(this)
+    this.categoryName = this.props.category.name.toLowerCase();
+  }
+
+  handleClick(e) {
+    let currPath = this.props.ownProps.match.path 
+    let nextPath = currPath.concat(`/${this.categoryName}`)
+    this.props.ownProps.history.push(nextPath)
   }
 
   render() {
     return (
-      <li className="class-category-item">
-        <div className={`cc-item cc-item-${this.props.category.name.toLowerCase()}`}>
+      <li onClick={this.handleClick} className="class-category-item">
+        <div className={`cc-item cc-item-${this.categoryName}`}>
           <h2 className="category-item-title">{this.props.category.name}</h2>
         </div>
       </li>
