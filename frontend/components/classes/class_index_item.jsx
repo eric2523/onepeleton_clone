161e5736c28eb24261d4fce5fc1d3873285e7411
eric2523@gmail.com <eric2523@gmail.com>
2020-10-13 19:35:54 -0700

    Add styling to classes page

--- frontend/components/classes/class_index_item.jsx
+++ frontend/components/classes/class_index_item.jsx
@@ -7,15 +7,16 @@ class ClassIndexItem extends React.Component {
 
   render(){
     let path = this.props.category.concat(this.props.classId)
+    let date = new Date(this.props.workoutClass.date).toUTCString()
 
     return(
       <li className="class-category-item class-list-cc">
         <div className={`cc-item-class cc-item-${path}`}>
           <div className="class-content-items">
-            <h2>{this.props.workoutClass.skillLevel}</h2>
+            <h2>{this.props.workoutClass.skillLevel.toUpperCase()}</h2>
             <h2>{this.props.workoutClass.name}</h2>
             <h2>{this.props.category.toUpperCase()}</h2>
-            <h2>{this.props.workoutClass.date}</h2>
+            <h2>{date}</h2>
           </div>
         </div>
       </li>
