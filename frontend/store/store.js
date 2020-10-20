b25457ddc5fa29a91935950bb12bc9156451716f
eric2523@gmail.com <eric2523@gmail.com>
2020-10-20 11:59:50 -0700

    Edit store to only have logger when not in production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,10 +4,15 @@ import logger from 'redux-logger'
 import rootReducer from '../reducers/root_reducer'
 
 const configureStore = (preloadedSate = {}) => {
+  let middleware = [thunk];
+  if (process.env.NODE_ENV !== 'production') {
+    middleware = [...middleware, logger];
+  }
+
   return createStore(
     rootReducer,
     preloadedSate,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middleware)
   );
 }
 
