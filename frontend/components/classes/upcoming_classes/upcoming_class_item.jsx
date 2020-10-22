18d5959e1837ebd0933103d0e22241dd373bf7b2
eric2523@gmail.com <eric2523@gmail.com>
2020-10-21 22:10:24 -0700

    Fix upcoming classes time to display better looking time

--- frontend/components/classes/upcoming_classes/upcoming_class_item.jsx
+++ frontend/components/classes/upcoming_classes/upcoming_class_item.jsx
@@ -1,62 +1,65 @@
-import React from 'react'
-import CancelBtn from './buttons/cancel_button';
-import SignUpBtn from './buttons/signup_button'
+import React from "react";
+import CancelBtn from "./buttons/cancel_button";
+import SignUpBtn from "./buttons/signup_button";
 
 class UpcomingClassesItem extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
-  getClassTime(classDate){
-    let hours = classDate.getHours()
-    let minutes = classDate.getMinutes()
-    
-    if (minutes < 10){
-      minutes = `0${minutes}`
+  getClassTime(classDate) {
+    let hours = classDate.getHours();
+    let minutes = classDate.getMinutes();
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
     }
-    
-    let day = classDate.getDay()
+
+    let day = classDate.getDay();
 
     switch (day) {
       case 0:
         day = "SUN";
-        break;    
+        break;
       case 1:
         day = "MON";
         break;
       case 2:
-        day = "TUES"
+        day = "TUES";
         break;
       case 3:
-        day = "WED"
+        day = "WED";
         break;
       case 4:
-        day = "THURS"
+        day = "THURS";
         break;
-      case 5: 
+      case 5:
         day = "FRI";
         break;
       case 6:
-        day = "SAT"
+        day = "SAT";
         break;
     }
 
-    let ampm = (hours < 12) ? "AM" : "PM"
+    let ampm = hours < 12 ? "AM" : "PM";
+    if (hours === 0) {
+      hours = 12;
+    } else if (hours > 12) {
+      hours -= 12;
+    }
 
-     let dateString = `${day} ${hours}:${minutes} ${ampm}`
-     return dateString;
+    let dateString = `${day} ${hours}:${minutes} ${ampm}`;
+    return dateString;
   }
 
-  getHoursTillStart(classDate){
-    let timeBetween = classDate - Date.now()
-    return (
-      Math.floor(timeBetween/(1000 * 60 * 60))
-    )
+  getHoursTillStart(classDate) {
+    let timeBetween = classDate - Date.now();
+    return Math.floor(timeBetween / (1000 * 60 * 60));
   }
 
-  render(){
+  render() {
     let submitBtn = null;
-    if (this.props.followingClass){
+    if (this.props.followingClass) {
       submitBtn = (
         <CancelBtn
           processBtn={this.props.unfollowClass}
@@ -76,12 +79,14 @@ class UpcomingClassesItem extends React.Component {
       );
     }
 
-    let classDate = new Date(this.props.workoutClass.date)
-    let classTime = this.getClassTime(classDate)
-    let timeBetween = this.getHoursTillStart(classDate)
+    let classDate = new Date(this.props.workoutClass.date);
+    let classTime = this.getClassTime(classDate);
+    let timeBetween = this.getHoursTillStart(classDate);
 
     return (
-      <div className={`upcoming-class-item-div upc-item-${this.props.categoryName}`}>
+      <div
+        className={`upcoming-class-item-div upc-item-${this.props.categoryName}`}
+      >
         <div className="upc-class-time">
           <span>{classTime}</span>
         </div>
@@ -100,8 +105,5 @@ class UpcomingClassesItem extends React.Component {
     );
   }
 }
-
-
-
 
 export default UpcomingClassesItem;
