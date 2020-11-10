be9e4970a887ea6924c511e0c0a2d26182d2fd7a
eric2523@gmail.com <eric2523@gmail.com>
2020-11-09 16:06:12 -0800

    Add favicon

--- frontend/components/profile/user_total_workouts_index.jsx
+++ frontend/components/profile/user_total_workouts_index.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import ProfCategoryList from './prof_category_list';
-import {carrotRightSVG} from '../svgs/profile_over_svg'
-import { Link } from 'react-router-dom'
+import React from "react";
+import ProfCategoryList from "./prof_category_list";
+import { carrotRightSVG } from "../svgs/profile_over_svg";
+import { Link } from "react-router-dom";
 
 class UserTotalWorkoutsIndex extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
-  componentDidMount(){
-    this.props.fetchAllCategories()
+  componentDidMount() {
+    this.props.fetchAllCategories();
   }
 
-  render(){
-    let workoutCounts = Object.keys(this.props.userClasses).length
+  render() {
+    let workoutCounts = Object.keys(this.props.userClasses).length;
     let linkpath = "/profile/workouts";
-    if (this.props.currUser !== this.props.sessionUser){
-      linkpath = `/profile/workouts/${this.props.currUser.id}`
+    if (this.props.currUser !== this.props.sessionUser) {
+      linkpath = `/profile/workouts/${this.props.currUser.id}`;
     }
 
     return (
@@ -28,21 +28,18 @@ class UserTotalWorkoutsIndex extends React.Component {
                 <span className="followers-count workouts-count">
                   {workoutCounts}
                 </span>
-                <h1 className="prof-follows">
-                  Total Workouts
-                </h1>
+                <h1 className="prof-follows">Total Workouts</h1>
               </div>
 
               <div className="view-workouts-div">
                 <span className="view-workouts-title">View Workouts</span>
-                <span className="carrot-svg">{ carrotRightSVG() }</span>
+                <span className="carrot-svg">{carrotRightSVG()}</span>
               </div>
             </div>
           </Link>
 
           <div className="prof-workouts-bottom">
-            <ProfCategoryList 
-              categories={this.props.categories} 
+            <ProfCategoryList categories={this.props.categories}
               workoutClasses={this.props.workoutClasses}
               fetchAllCategories={this.props.fetchAllCategories}
             />
