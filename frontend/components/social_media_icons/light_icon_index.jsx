dc937c0a7c9083a9bd21429f6462a6c5fd77fee8
eric2523@gmail.com <eric2523@gmail.com>
2020-11-10 16:54:29 -0800

    Open icon links in new tab

--- frontend/components/social_media_icons/light_icon_index.jsx
+++ frontend/components/social_media_icons/light_icon_index.jsx
@@ -5,22 +5,22 @@ const LightIconIndex  = () => {
     <>
       <ul className="media-icons-list">
         <li key="1">
-          <a href="https://www.facebook.com/onepeloton">
+          <a target="_blank" href="https://www.facebook.com/onepeloton">
             <i className="fab fa-facebook-square media-icons"></i>
           </a>
         </li>
         <li key="2">
-          <a href="https://www.instagram.com/onepeloton">
+          <a target="_blank" href="https://www.instagram.com/onepeloton">
             <i className="fab fa-instagram media-icons"></i>
           </a>
         </li>
         <li key="3">
-          <a href="https://twitter.com/onepeloton">
+          <a target="_blank" href="https://twitter.com/onepeloton">
             <i className="fab fa-twitter media-icons"></i>
           </a>
         </li>
         <li key="4">
-          <a href="https://www.youtube.com/c/onepeloton">
+          <a target="_blank" href="https://www.youtube.com/c/onepeloton">
             <i className="fab fa-youtube media-icons"></i>
           </a>
         </li>
