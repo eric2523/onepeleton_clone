46bab37d78cb6d37d39ddb5dd1aa0278429435f3
eric2523@gmail.com <eric2523@gmail.com>
2020-11-18 11:24:04 -0800

    Change carrots on schedule nav

--- frontend/components/schedule/schedule_nav_bar.jsx
+++ frontend/components/schedule/schedule_nav_bar.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import ScheduleNavbarItem from './schedule_navbar_item';
-import { carrotLeftSVG, carrotRightSVG } from '../svgs/profile_over_svg';
+import React from "react";
+import ScheduleNavbarItem from "./schedule_navbar_item";
+// import { carrotLeftSVG, carrotRightSVG } from "../svgs/profile_over_svg";
 
 class ScheduleNavBar extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
-  render(){
-   let listItems =  this.props.navIds.map((navItem, idx) => (
-      <ScheduleNavbarItem key={idx} navItem={navItem}/>
-    ))
+  render() {
+    let listItems = this.props.navIds.map((navItem, idx) => (
+      <ScheduleNavbarItem key={idx} navItem={navItem} />
+    ));
 
-    return(
+    return (
       <div className="schedule-nav-bar">
         <div className="sched-carrot-left">
-          {carrotLeftSVG()}
+          <img src="https://img.icons8.com/fluent-systems-filled/24/000000/horizontal-line.png" />
         </div>
-        <ul className="sched-nav-bar-ul">
-          {listItems}
-        </ul>
+        <ul className="sched-nav-bar-ul">{listItems}</ul>
         <div className="sched-carrot-left">
-          {carrotRightSVG()}
+          <img src="https://img.icons8.com/fluent-systems-filled/24/000000/horizontal-line.png" />
         </div>
       </div>
-    )
+    );
   }
 }
 
